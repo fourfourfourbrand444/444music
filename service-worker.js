6ec9fef33b273b935b1a180b76ef9d613eaaf0ec
fourfourfourbrand444 <fourfourfourbrand444@gmail.com>
2026-02-27 22:57:11 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "444music-app-v1";
+
+const urlsToCache = [
+  "/",
+  "/view.html",
+  "/style.css",
+  "/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
